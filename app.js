3e78eac3f9264ea3dde1e90862896d194b473b5e
R0777 <romka256@yandex.ru>
2020-10-08 23:59:41 +0300

    express: server-fix-fich

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const users = require('./routes/users.js');
 const { PORT = 3000} = process.env;
 const app = express();
 
-app.use('/', (req, res) => {
-  res.status(404).send({"message": "Запрашиваемый ресурс не найден"});
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', users);
 app.use('/', cards);
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/', (req, res) => {
+  res.status(404).send({"message": "Запрашиваемый ресурс не найден"});
+});
 
 app.listen(PORT, () => {
   console.log(`ON`)
